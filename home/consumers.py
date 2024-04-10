688dd36360e6c74f0f94def5d08753b580928218
nitesh-segno <nitesh@segnotech.com>
2024-04-10 22:05:24 +0530

    changes

--- home/consumers.py
+++ home/consumers.py
@@ -4,6 +4,25 @@ from .models import *
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
 from asgiref.sync import sync_to_async
+import os 
+from django.conf import settings
+import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+import warnings
+
+def AIResponse(text):
+    warnings.filterwarnings('ignore')
+    "cohere api"
+    "Google api"
+
+    if "GOOGLE_API_KEY" not in os.environ:
+        os.environ["GOOGLE_API_KEY"] = settings.GOOGLE_KEY 
+
+    llm =ChatGoogleGenerativeAI(model='gemini-pro',convert_system_message_to_human=True)
+    result = llm.invoke(text)
+
+    print(result.content)
+    return result.content
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -24,24 +43,30 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         user = text_data_json["user"]
+        bot_res = AIResponse(message)
 
-        dbsv = await self.save_chat(message, user)
+        dbsv = await self.save_chat(message, user, bot=False)
+        await self.save_chat(bot_res, 3, bot=True)
         cid = dbsv['id']
         user_id = self.scope["user"].id
         usr = await sync_to_async(User.objects.get)(id=user_id)
         # Send message to room group
         await self.channel_layer.group_send(
-            self.room_group_name, {"type": "chat.message", "message": message, 'user':usr.username, 'id':cid}
+            self.room_group_name, {"type": "chat.message", "message": message, 'user':usr.username, 'id':cid, "bot_res":bot_res}
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event["message"]
+        bot_res1 = event["bot_res"]
 
         # Send message to WebSocket
-        await self.send(text_data=json.dumps({"message": message,'user':event["user"]}))
+        await self.send(text_data=json.dumps({"message": message,'user':event["user"],"bot_res":bot_res1}))
 
     @database_sync_to_async
-    def save_chat(self, message, user):
-        cht = Chat.objects.create(sender=User.objects.get(id=user), channel=Channel.objects.get(id=self.room_name), message=message)
+    def save_chat(self, message, user, bot):
+        if bot:
+            cht = Chat.objects.create(sender=User.objects.get(id=user), channel=Channel.objects.get(id=self.room_name), message=message)
+        else:
+            cht = Chat.objects.create(sender=User.objects.get(id=user), channel=Channel.objects.get(id=self.room_name), message=message)
         return {"id":cht.id}
